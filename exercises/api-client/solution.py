cbe3b2e01a7f91c826ac804a561b30452a7bacb2
Mike Rossetti <s2t2@users.noreply.github.com>
2019-06-12 16:52:36 -0400

    Add solution for product creation

--- exercises/api-client/solution.py
+++ exercises/api-client/solution.py
@@ -14,24 +14,20 @@ def get_product(product_id):
     #print(response.status_code)
     return json.loads(response.text)
 
-#
-# DRAFT CODE NEEDS TESTING AND REVISION...
-#
-#def create_product(product_attrs):
-#    print("CREATING A NEW PRODUCT...")
-#    request_url = "https://groceries-api-csv.herokuapp.com/products"
-#    response = requests.post(request_url, json=product_attrs)
-#    print(response.status_code)
-#    return json.loads(response.text)
+def create_product(product_attrs):
+    request_url = "https://groceries-api-csv.herokuapp.com/products"
+    response = requests.post(request_url, json=product_attrs)
+    #print(response.status_code)
+    return json.loads(response.text)
+
+# TODO...
 #
 #def update_product(product_id, product_attrs):
-#    print(f"UPDATING PRODUCT #{product_id}...")
 #    request_url = f"https://groceries-api-csv.herokuapp.com/products/{product_id}"
 #    response = requests.put(request_url, json=product_attrs)
 #    print(response.status_code)
 #
 #def delete_product(product_id):
-#    print(f"DELETING PRODUCT #{product_id}...")
 #    request_url = f"https://groceries-api-csv.herokuapp.com/products/{product_id}"
 #    response = requests.delete(request_url)
 #    print(response.status_code)
@@ -53,3 +49,23 @@ if __name__ == "__main__":
 
     product = get_product(product_id)
     print(product)
+
+    print("------------")
+    print(f"CREATING A NEW PRODUCT...")
+
+    product_ids = [int(p["id"]) for p in products]
+    next_id = max(product_ids) + 1
+    new_product_attrs = {
+        "id": next_id,
+        "name":"My New Product",
+        "price":"99.5",
+        "aisle":"cookies cakes",
+        "department":"snacks",
+    }
+    print(new_product_attrs)
+
+    new_product = create_product(new_product_attrs)
+
+    print(new_product)
+
+    #breakpoint()
