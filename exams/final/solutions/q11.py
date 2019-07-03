365b19b1a87164269d84980c31bf90fd129498d2
Mike Rossetti <s2t2@users.noreply.github.com>
2019-07-03 14:36:51 -0400

    Exam solutions (#203)
    * Fix quote characters
    
    * Submit my other project
    
    * Post exam PDF
    
    * Post solution for exam question 12
    
    * Post solutions for question 12 parts a,b,c
    
    * More solutions
    
    * More solutions
    
    * Post solutions and fix quotes
    
    * Add solution and fix single quotes
    
    * Add alternative solution
    
    * Reorder preference of solutions

---
+++ exams/final/solutions/q11.py
@@ -0,0 +1,73 @@
+
+salads = [
+    {"id": 1, "name": "Caesar", "price": 8.99},
+    {"id": 2, "name": "Chicken Caesar", "price": 11.99},
+    {"id": 3, "name": "Waldorf", "price": 10.99},
+    {"id": 4, "name": "Cobb", "price": 9.99},
+    {"id": 5, "name": "Caprese", "price": 9.99},
+    {"id": 6, "name": "Nicoise", "price": 10.99},
+]
+
+#
+# QUESTION 11-A
+#
+# Assuming the identifier, or ​"id"​ attribute, of each salad is and will always be unique,
+# ... and assuming the order of salads may vary,
+# ... "print" the name of the salad whose identifier is equal to ​3​ (i.e. ​"Waldorf"​):
+#
+
+# SOLUTION:
+
+matching_salads = [salad for salad in salads if salad["id"] == 3]
+matching_salad = matching_salads[0]
+print(matching_salad["name"])
+
+# ALTERNATIVE SOLUTION:
+
+matching_salads = []
+
+for salad in salads:
+    if salad["id"] == 3:
+        matching_salads.append(salad)
+
+matching_salad = matching_salads[0]
+print(matching_salad["name"])
+
+# ALTERNATIVE SOLUTION:
+
+for salad in salads:
+    if salad["id"] == 3:
+        print(salad["name"])
+
+#
+# QUESTION 11-B
+#
+# Assuming the ​"price"​ attribute represents a salad's cost to the consumer,
+# ... "print" the number of salads which are more expensive than ten dollars (i.e. ​3​):
+#
+
+# SOLUTION:
+
+expensive_salads = [salad for salad in salads if salad["price"] > 10]
+
+print(len(expensive_salads))
+
+# ALTERNATIVE SOLUTION:
+
+expensive_salads = []
+
+for salad in salads:
+    if salad["price"] > 10:
+        expensive_salads.append(salad)
+
+print(len(expensive_salads))
+
+# ALTERNATIVE SOLUTION:
+
+n = 0
+
+for salad in salads:
+    if salad["price"] > 10:
+        n = n + 1
+
+print(n)
---
+++ exams/final/solutions/rideshare.py
@@ -0,0 +1,21 @@
+trip = {
+    "driver": {
+        "first_name": "Danny",
+        "last_name": "Dreyfus",
+        "avg_rating": 3.6,
+        "total_rides": 950
+    },
+    "vehicle": {
+        "make": "Toyota",
+        "model": "Prius",
+        "year": 2015,
+        "color": "cherry red"
+    },
+    "rideshare": True,
+    "pickup_location": "Grand Central Terminal",
+    "stops": [
+        {"sequence": 1, "passenger": "Vishal", "destination": "Madison Square", "fare": 3.99},
+        {"sequence": 2, "passenger": "Clara", "destination": "Union Square", "fare": 5.99},
+        {"sequence": 3, "passenger": "Lee", "destination": "Washington Square", "fare": 7.99}
+    ]
+}
